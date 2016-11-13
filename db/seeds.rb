5ed9de0e32d85dc6e3b8a474a437ca474445d17a
Inou Ridder <inouridder@gmail.com>
2016-11-13 18:45:20 +0100

    Added Functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+Movie.create(
+  rating: 5,
+  title: "Stefan and his Great Blue Shirt"
+  )
