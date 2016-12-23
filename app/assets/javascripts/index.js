74b0f7013c35a9bb7c822dce6e09f6ba512f8a20
Inou Ridder <inouridder@gmail.com>
2016-12-23 19:24:20 +0100

    la

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-baseUrl = 'http://localhost:3000/'
+baseUrl = 'https://veganmoviecritic.herokuapp.com/'
 
 $(document).ready(function(){
   $(".super-btn").click(function(){
@@ -9,12 +9,8 @@ $(document).ready(function(){
       type: 'GET',
       url: baseUrl + id,
       success: function(data) {
-        $.post("getstuff.php", function(data){
-        var mainDiv = $("#mainDiv", data); // finds <div id='mainDiv'>...</div>
-        }, "html");
         $('.content-review-index').html("");
         $('.content-review-index').append(data);
-        $('.card').removeClass('hidden');
       },
       error: function(html){
         console.log(html);
@@ -23,20 +19,20 @@ $(document).ready(function(){
   });
 });
 
-window.onload = function(){
-  var items = $('.card');
-  $('.movie-info').append("I FOUND YOU");
-  var currentItem = items.filter('.active');
-  $(".next-button").on('click', function() {
-    console.log("hello world");
-    var nextItem = currentItem.next();
-    currentItem.removeClass('active');
-    if ( nextItem.length ) {
-      currentItem = nextItem.addClass('active');
-    } else {
-        // If you want it to loop around
-        currentItem = items.first().addClass('active');
-      };
-    });
-};
+// window.onload = function(){
+//   var items = $('.card');
+//   $('.movie-info').append("I FOUND YOU");
+//   var currentItem = items.filter('.active');
+//   $(".next-button").on('click', function() {
+//     console.log("hello world");
+//     var nextItem = currentItem.next();
+//     currentItem.removeClass('active');
+//     if ( nextItem.length ) {
+//       currentItem = nextItem.addClass('active');
+//     } else {
+//         // If you want it to loop around
+//         currentItem = items.first().addClass('active');
+//       };
+//     });
+// };
 
