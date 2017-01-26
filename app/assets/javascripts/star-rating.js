6179e71c094505f99b087b673cddf02c532e45cb
Inou Ridder <inouridder@gmail.com>
2017-01-26 17:04:57 +0100

    added stars and css, to do: create ajax post and gets for setting stars per review

---
+++ app/assets/javascripts/star-rating.js
@@ -0,0 +1,13 @@
+// $('.star-rating').click(function(){
+//   var value = $('.star-rating').val();
+//         $.ajax({
+//       type: 'POST',
+//       url: review_ratings_path,
+//       success: function(data) {
+//         $('.content-review-index').append(data);
+//       },
+//       error: function(html){
+//         console.log(html);
+//       }
+//     })
+// });
