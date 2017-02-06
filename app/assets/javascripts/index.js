c691e12b67605f5fb466b5453763130abeb7ee42
Inou Ridder <inouridder@gmail.com>
2017-02-06 15:17:29 +0100

    heroku fix index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
 // CHANGE WHEN PUSHING TO HEROKU!
-baseUrl = 'http://localhost:3000/'
-// baseUrl = 'https://veganmoviecritic.herokuapp.com'
+// baseUrl = 'http://localhost:3000/'
+baseUrl = 'https://veganmoviecritic.herokuapp.com'
 
 $(document).ready(function(){
   $(".click-btn").click(function(){
