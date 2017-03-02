c9c594345676a82caa9aac50d32f146669e80d87
Inou Ridder <inouridder@gmail.com>
2017-03-02 21:02:37 +0100

    Add styling changes - movie#show, add function for next#button

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $(".next-btn").click(function(){
     $(".next-method").next().removeClass("hidden");
     $(".next-method").prev().addClass("hidden");
+    $('body, html').animate({
+     scrollTop: 0},700);
   });
 
   $(".prev-btn").click(function(){
@@ -10,5 +12,7 @@ $(document).ready(function(){
     $(".next-method").next().addClass("hidden");
     $(".next-btn").removeClass("hidden");
     $(".col-xs-2 pull-right").removeClass("hidden");
+    $('body, html').animate({
+     scrollTop: 0},700);
   });
 });
