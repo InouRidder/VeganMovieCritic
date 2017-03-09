1093f856cfaea280bd7ce835c5c5bebe3e9ea8df
Inou Ridder <inouridder@gmail.com>
2017-03-09 19:08:42 +0100

    Add Profile dropdown

--- app/assets/javascripts/word_count.js
+++ app/assets/javascripts/word_count.js
@@ -2,12 +2,12 @@ $(document).ready(function(e) {
 
   $('.btn-save').on('click', function(e) {
 
-    var title_words = $('#review_title').val().trim().replace(/\s+/gi, ' ').split(' ')
+    var title_words = $('#review_title').val().trim().replace(/\s+/gi, ' ')
 
-    if (( title_words.length > 20 ) || ((title_words.length-1) < 1 )) {
+    if (( title_words.length > 20 ) || ((title_words.length - 1) < 1 )) {
       $('#review_title-error').empty().append('Please supply a title!');
       e.preventDefault();
-      $("html, body").animate({ scrollTop: 190 }, 600);
+      $("html, body").animate({ scrollTop: 0 }, 600);
 
     }
 
@@ -15,8 +15,8 @@ $(document).ready(function(e) {
 
     if ((content_words.length > 111)  || (content_words.length < 50 )) {
       e.preventDefault();
-      $('.word-counter-error').empty().append('Please note that the length of the review must fall between 50 and 110 words');
-      $('.current-word-count').empty().append(content_words.length);
+      $('.word-counter-error').empty().append('Please note that the length of the review must be between 50 and 110 words');
+      $('.current-word-count').empty().append('Current amount words: ' + content_words.length + ' of 110');
       $("html, body").animate({ scrollTop: 190 }, 600);
     }
 
