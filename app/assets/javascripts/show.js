f8a1379b7328e681d0249366b3ca44c537866b4c
Inou Ridder <inouridder@gmail.com>
2017-03-12 16:02:18 +0100

    Fix next method

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-
   $(".next-btn").click(function(){
-    $(".next-method").next().removeClass("hidden");
-    $(".next-method").prev().addClass("hidden");
+    var element = $(".current-element").next(".next-method");
+    $(".next-method").addClass("hidden").removeClass("current-element");
+    element.removeClass('hidden').addClass("current-element");
+    console.log(element);
     $('body, html').animate({
      scrollTop: 0},700);
   });
 
   $(".prev-btn").click(function(){
-    $(".next-method").prev().removeClass("hidden");
-    $(".next-method").next().addClass("hidden");
-    $(".next-btn").removeClass("hidden");
-    $(".col-xs-2 pull-right").removeClass("hidden");
+   var element = $(".current-element").prev(".next-method");
+    $(".next-method").addClass("hidden").removeClass("current-element");
+    element.removeClass('hidden').addClass('current-element');
+    console.log(element);
     $('body, html').animate({
      scrollTop: 0},700);
   });
 });
+
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
       review.set_rating
       review.save!
     end
-    @reviews = @movie.reviews.where(approved: true).order(rating: :desc)
+    @reviews = @movie.reviews.where(approved: true).order(review_rating: :desc)
   end
 
   def new
@@ -95,7 +95,7 @@ class MoviesController < ApplicationController
   end
 
   def partial
-    @review = @movie.reviews.where(approved: :true).where.not(review_rating: nil).order(review_rating: :desc).first
+    @review = @movie.reviews.where(approved: :true).where.not(rating: nil).order(review_rating: :desc).first
     @user = current_user
     if @review && current_user
       @review_rating = ReviewRating.where(review_id: @review.id).where(user_id: @user.id).first || ReviewRating.new
