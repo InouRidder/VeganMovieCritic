91308b386e855f5e94fb21269ede35646c19d2dc
Inou Ridder <inouridder@gmail.com>
2017-03-24 17:45:58 +0100

    add favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_filter :add_allow_credentials_headers
     # [...]
     before_action :authenticate_user!
     include Pundit
@@ -16,9 +17,23 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  private
+#   def add_allow_credentials_headers
+#   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5
+#   # Because we want our front-end to send cookies to allow the API to be authenticated
+#   # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so
+#   # the browser will not reject the response
+#     response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+#     response.headers['Access-Control-Allow-Credentials'] = 'true'
+#   end
+
+# def options
+#   head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
+# end
+
+rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
