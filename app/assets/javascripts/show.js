55f583a69d94f56dc89a0d65416584db9aef460d
Inou Ridder <inouridder@gmail.com>
2018-09-19 17:35:01 +0800

    fix movie show bug

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -3,7 +3,6 @@ document.addEventListener('turbolinks:load', function() {
     var element = $(".current-element").next(".next-method");
     $(".next-method").addClass("hidden").removeClass("current-element");
     element.removeClass('hidden').addClass("current-element");
-    console.log(element);
     $('body, html').animate({
      scrollTop: 0},700);
   });
@@ -12,7 +11,6 @@ document.addEventListener('turbolinks:load', function() {
    var element = $(".current-element").prev(".next-method");
     $(".next-method").addClass("hidden").removeClass("current-element");
     element.removeClass('hidden').addClass('current-element');
-    console.log(element);
     $('body, html').animate({
      scrollTop: 0},700);
   });
