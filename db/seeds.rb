e5a22e6512b671a198ec696ca1dfc7afa5e65e56
Inou Ridder <inouridder@gmail.com>
2018-09-21 13:53:22 +0800

    last addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Movie.destroy_all
-
-Movie.create(
-  rating: 5,
-  title: "Stefan and his Great Blue Shirt"
-  )
